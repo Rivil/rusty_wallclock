dde33ed729710e3c4c09a96d36fbc56cd30e4049
rivil <rivil@rivil.co.uk>
2023-05-28 10:25:11 +0100

    rocket and yew together

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::fs::NamedFile;
+use rocket::response::status::NotFound;
+use std::path::PathBuf;
+
+#[macro_use]
+extern crate rocket;
+
+async fn get_index() -> Result<NamedFile, NotFound<String>> {
+    NamedFile::open("../ui/dist/index.html").await.map_err(|e| NotFound(e.to_string()))
+}
+
+#[get("/<path..>")]
+async fn static_files(path: PathBuf) -> Result<NamedFile, NotFound<String>> {
+    let path = PathBuf::from("../ui/dist/").join(path);
+    match NamedFile::open(path).await {
+        Ok(file) => Ok(file),
+        Err(_) => get_index().await,
+    }
+}
+
+#[get("/")]
+async fn index() -> Result<NamedFile, NotFound<String>> {
+    get_index().await
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, static_files])
 }
